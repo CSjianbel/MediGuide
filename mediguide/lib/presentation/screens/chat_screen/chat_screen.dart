62e7802ae05886db508ee88ba153200bd91a1422
CSjianbel <jiankarlo99@gmail.com>
2023-12-10 19:44:45 +0800

    feat: deleted chat_input_section.dart, can send messages as user

--- mediguide/lib/presentation/screens/chat_screen/chat_screen.dart
+++ mediguide/lib/presentation/screens/chat_screen/chat_screen.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:mediguide/presentation/screens/chat_screen/widgets/app_bar.dart';
 import 'package:mediguide/presentation/screens/chat_screen/widgets/user_chat_bubble.dart';
 import 'package:mediguide/presentation/screens/chat_screen/widgets/bot_chat_bubble.dart';
-import 'package:mediguide/presentation/screens/chat_screen/widgets/chat_input_section.dart';
 import 'package:mediguide/presentation/screens/chat_screen/widgets/navigation_drawer.dart'
     as navigation_drawer;
+import 'package:mediguide/utils/theme_utils.dart';
+import 'package:mediguide/utils/theme_constants.dart';
 
 class Message {
   final String message;
@@ -22,6 +23,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController messageController = TextEditingController();
   final List<Message> messages = [
     Message(
         bot: true,
@@ -43,6 +45,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData currentTheme = ThemeUtils.getTheme(context);
     return Scaffold(
         appBar: const PreferredSize(
           preferredSize: Size.fromHeight(70),
@@ -62,7 +65,65 @@ class _ChatScreenState extends State<ChatScreen> {
                         }
                         return UserChatBubble(message: messages[index].message);
                       })),
-              const ChatInputSection(),
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                color: currentTheme.appBarTheme.backgroundColor,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 45,
+                        child: TextFormField(
+                          controller: messageController,
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 0),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color:
+                                    ThemeUtils.getIconBackground(currentTheme),
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            hintText: 'How are you feeling today?',
+                            hintStyle: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 12,
+                            ),
+                          ),
+                          style: const TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 15.0),
+                    Container(
+                      width: 45,
+                      height: 45,
+                      decoration: const BoxDecoration(
+                          color: accentColor,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0))),
+                      child: IconButton(
+                        icon: const Icon(Icons.send,
+                            color: lightForeground, size: 20),
+                        onPressed: () {
+                          setState(() {
+                            messages.add(Message(
+                                bot: false, message: messageController.text));
+                            FocusScope.of(context).unfocus();
+                            messageController.text = '';
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
